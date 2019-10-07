e7159ca73ee7918d2c0d5e44b84cfa72e63ae52d
Mamadou Wann <mamadouwann@gmail.com>
2019-10-07 19:01:12 -0400

    first commit
    commiting everything

---
+++ TechTalentTwitter/src/main/java/com/tts/TechTalentTwitter/TechTalentTwitterApplication.java
@@ -0,0 +1,25 @@
+package com.tts.TechTalentTwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechTalentTwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechTalentTwitterApplication.class, args);
+		System.out.println(twitter);
+	}
+	
+	private static final String twitter = 
+			"████████╗███████╗ ██████╗██╗  ██╗    ████████╗ █████╗ ██╗     ███████╗███╗   ██╗████████╗    ████████╗██╗    ██╗██╗████████╗████████╗███████╗██████╗ \n" + 
+			"╚══██╔══╝██╔════╝██╔════╝██║  ██║    ╚══██╔══╝██╔══██╗██║     ██╔════╝████╗  ██║╚══██╔══╝    ╚══██╔══╝██║    ██║██║╚══██╔══╝╚══██╔══╝██╔════╝██╔══██╗\n" + 
+			"   ██║   █████╗  ██║     ███████║       ██║   ███████║██║     █████╗  ██╔██╗ ██║   ██║          ██║   ██║ █╗ ██║██║   ██║      ██║   █████╗  ██████╔╝\n" + 
+			"   ██║   ██╔══╝  ██║     ██╔══██║       ██║   ██╔══██║██║     ██╔══╝  ██║╚██╗██║   ██║          ██║   ██║███╗██║██║   ██║      ██║   ██╔══╝  ██╔══██╗\n" + 
+			"   ██║   ███████╗╚██████╗██║  ██║       ██║   ██║  ██║███████╗███████╗██║ ╚████║   ██║          ██║   ╚███╔███╔╝██║   ██║      ██║   ███████╗██║  ██║\n" + 
+			"   ╚═╝   ╚══════╝ ╚═════╝╚═╝  ╚═╝       ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝╚═╝  ╚═══╝   ╚═╝          ╚═╝    ╚══╝╚══╝ ╚═╝   ╚═╝      ╚═╝   ╚══════╝╚═╝  ╚═╝\n" + 
+			"                                                                                                                                                     \n" + 
+			"";
+
+}
+
